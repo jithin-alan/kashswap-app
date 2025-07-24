015aa54e79f65601e266a5e37614811b01612e55
Jithin Mohan <jithinmohan10@gmail.com>
2025-07-24 14:44:46 +0000

    take me trough steps to add the capacitor to this project i will add it.

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.globaldev.kashswap',
+  appName: 'KashSwap',
+  webDir: 'out',
+  bundledWebRuntime: false,
+};
+
+export default config;
